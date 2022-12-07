c02ce8b808109751369909ac94a806eebf48a65a
Philip Dubé <phdub@microsoft.com>
2022-12-07 01:15:01 +0000

    Fix, make beep count configurable

--- main.js
+++ main.js
@@ -22,33 +22,61 @@ function beep(duration, frequency, volume, type, callback) {
 	oscillator.stop(audio.currentTime + duration);
 }
 
-const countput = document.getElementById("count");
+const freqput = document.getElementById("freqs");
+const countsdiv = document.getElementById("counts");
 const startbtn = document.getElementById("start");
 const promptdiv = document.getElementById("prompt");
-let actualCount = 0;
+let actualCounts = [],
+	freqCount = 0;
 const pending = [];
 startbtn.addEventListener("click", function (e) {
 	if (this.value === "Start") {
+		while (countsdiv.lastChild) countsdiv.removeChild(countsdiv.lastChild);
 		audio ||= new AudioContext();
-		actualCount = 0;
-		for (let i = 0; i < 30000; i += 300 + Math.random() * 1000) {
-			pending.push(
-				setTimeout(() => {
-					console.log("beep");
-					beep(200, 300, 1.0, "sine");
-				}, i)
-			);
+		actualCounts.length = 0;
+		freqCount = freqput.value;
+		const taken = new Set();
+		for (let f = 0; f < freqCount; f++) {
+			let actualCount = 0;
+			for (
+				let i = 0;
+				i < 30000;
+				i += Math.random() * Math.random() * 2000 * freqCount
+			) {
+				if (taken.has(i)) continue;
+				for (let j = i - 40; j < i + 40; j++) {
+					taken.add(j);
+				}
+				actualCount++;
+				pending.push(
+					setTimeout(
+						() => beep(0.06, 360 + (720 / freqCount) * f, 1.0, "sine"),
+						i
+					)
+				);
+			}
+			actualCounts.push(actualCount);
 		}
 		pending.push(
 			setTimeout(() => {
 				promptdiv.textContent = "How many beeps?";
+				for (let i = 0; i < freqCount; i++) {
+					const countpush = document.createElement("input");
+					countpush.type = "button";
+					countpush.value = "Beep";
+					countpush.onclick = () => beep(0.06, 240 + 80 * i, 1.0, "sine");
+					const count = document.createElement("input");
+					count.type = "input";
+					countsdiv.appendChild(countpush);
+					countsdiv.appendChild(count);
+					countsdiv.appendChild(document.createElement("br"));
+				}
 				this.value = "Guess";
 			}, 31000 + audio.outputLatency * 1000)
 		);
 		this.value = "Stop";
 	} else if (this.value === "Guess") {
-		const guess = countput.value | 0;
-		promptdiv.textContent = `Guess: ${countput.value}. Actual value: ${actualCount}`;
+		promptdiv.textContent = `Actual counts: ${actualCounts.join(", ")}`;
 		this.value = "Start";
 	} else if (this.value === "Stop") {
 		for (const timer of pending) {
